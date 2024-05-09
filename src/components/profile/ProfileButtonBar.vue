2c23776e1815094104339d009332e686ba272b02
Lean <kristsjavalds10@gmail.com>
2024-05-09 22:47:03 +0300

    idek at this point

--- src/components/profile/ProfileButtonBar.vue
+++ src/components/profile/ProfileButtonBar.vue
@@ -12,20 +12,16 @@
                 <v-tab value="three">Media</v-tab>
                 <v-tab value="four">Likes</v-tab>
                 </v-tabs>
-  <v-container>
-    <v-card class="interactions-card" rounded="lg">
-      <v-tabs v-model="tab" grow>
-        <v-tab value="one">Posts</v-tab>
-        <v-tab value="two">Replies</v-tab>
-        <v-tab value="three">Media</v-tab>
-        <v-tab value="four">Likes</v-tab>
-      </v-tabs>
 
-      <v-card-text>
-        <v-tabs-window v-model="tab">
-          <v-tabs-window-item value="one"> One </v-tabs-window-item>
+                <v-card-text>
+                <v-tabs-window v-model="tab">
+                    <v-tabs-window-item value="one">
+                    One
+                    </v-tabs-window-item>
 
-          <v-tabs-window-item value="two"> Two </v-tabs-window-item>
+                    <v-tabs-window-item value="two">
+                    Two
+                    </v-tabs-window-item>
 
                     <v-tabs-window-item value="three">
                     Three
